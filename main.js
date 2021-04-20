7dd49f9414b110ff22d59e7dc535502edb8a1c5e
Jonathan <contact@grafikart.fr>
2021-04-20 10:37:38 +0200

    feat: changement du titre

--- main.js
+++ main.js
@@ -3,24 +3,32 @@ import { Curtains, Plane } from "curtainsjs";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
 
+/**
+ * Implémentation naïve du markdown
+ *
+ * @param {string} str
+ * @returns {string}
+ */
+function microMarkdown(str) {
+  return str.replace(/\*\*([^*]*)\*\*?/g, "<strong>$1</strong>");
+}
+
+// On extrait les phrase depuis les paramètres de l'URL
 const url = new URL(window.location.href);
-
-const injectableParams = ["os", "ide"];
-
+const injectableParams = ["os", "ide", "baseline"];
 injectableParams.forEach((paramName) => {
-  if (url.searchParams.get(paramName)) {
-    document.getElementById(paramName).innerText = url.searchParams.get(
-      paramName
-    );
+  const paramValue = url.searchParams.get(paramName);
+  if (paramValue) {
+    document.getElementById(paramName).innerHTML = microMarkdown(paramValue);
   }
 });
 
+// On charge curtains pour l'effet "LavaLamp"
 const curtains = new Curtains({
   container: document.body,
   watchScroll: false,
   pixelRatio: Math.min(2, window.devicePixelRatio),
 });
-
 const plane = new Plane(curtains, document.getElementById("background"), {
   vertexShader,
   fragmentShader,
@@ -32,9 +40,7 @@ const plane = new Plane(curtains, document.getElementById("background"), {
     },
   },
 });
-
 const startTime = Date.now();
-
 plane.onRender(() => {
   plane.uniforms.uTime.value = Date.now() - startTime;
 });
