c68e2cc1c59e22aca84bc5fe9eb58de0260650a4
Jonathan <contact@grafikart.fr>
2023-01-05 12:10:13 +0100

    Updates

--- assets/layout.17b058e0.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill.b7f2da20.js";const o=window.location.hash.replace("#",""),a=`layout-${o===""?"default":o}`;document.body.classList.add(a);
---
+++ assets/layout.75dbca9c.js
@@ -0,0 +1,1 @@
+import"./layout.6aa687b7.js";const a=window.location.hash.replace("#",""),o=`layout-${a===""?"default":a}`;document.body.classList.add(o);
